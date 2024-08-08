7277950c32c5d23840e52e50d0d02622fbe64bbe
FranMaggi73 <franmaggi73@gmail.com>
2024-08-07 21:57:55 -0300

    b

--- svelte.config.js
+++ svelte.config.js
@@ -3,18 +3,16 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-  // for more information about preprocessors
   preprocess: vitePreprocess(),
 
   kit: {
     adapter: adapter({
-      pages: 'build', // Verifica que esto sea correcto
-      assets: 'build', // Verifica que esto sea correcto
+      pages: 'build',
+      assets: 'build',
       fallback: null
     }),
     paths: {
-      base: process.env.NODE_ENV === 'production' ? '/' : ''
+      base: process.env.NODE_ENV === 'production' ? '/portfolio-sveltekit' : ''
     },
     alias: {
       $lib: 'src/lib'
