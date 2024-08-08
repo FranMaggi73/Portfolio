f1d3238e713955a423701c8fd8d6fa34cc16e707
FranMaggi73 <franmaggi73@gmail.com>
2024-08-07 21:56:01 -0300

    a

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,7 @@ const config = {
       fallback: null
     }),
     paths: {
-      base: process.env.NODE_ENV === 'production' ? '/portfolio-sveltekit' : ''
+      base: process.env.NODE_ENV === 'production' ? '/' : ''
     },
     alias: {
       $lib: 'src/lib'
