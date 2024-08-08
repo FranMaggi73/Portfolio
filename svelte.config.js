f106a4b0d4be29a74f1c992bb798b52903ab7caa
FranMaggi73 <franmaggi73@gmail.com>
2024-08-07 21:23:30 -0300

    :bug: fixed config svelte

--- svelte.config.js
+++ svelte.config.js
@@ -17,7 +17,7 @@ const config = {
       fallback: null
     }),
     paths: {
-      base: process.env.NODE_ENV === 'production' ? '/portfolio' : ''
+      base: process.env.NODE_ENV === 'production' ? '/' : ''
     },
     alias: {
       $lib: 'src/lib'
