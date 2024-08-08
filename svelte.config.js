2d946bdec6f54043e28cbe0a29777bd6bcc18a3c
FranMaggi73 <franmaggi73@gmail.com>
2024-08-07 21:55:02 -0300

    :bug: fixed config

--- svelte.config.js
+++ svelte.config.js
@@ -8,10 +8,14 @@ const config = {
   preprocess: vitePreprocess(),
 
   kit: {
-    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
-    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter(),
+    adapter: adapter({
+      pages: 'build', // Verifica que esto sea correcto
+      assets: 'build', // Verifica que esto sea correcto
+      fallback: null
+    }),
+    paths: {
+      base: process.env.NODE_ENV === 'production' ? '/portfolio-sveltekit' : ''
+    },
     alias: {
       $lib: 'src/lib'
     }
