fa259f96d9d831b1a5e261935f46e993d23153b6
FranMaggi73 <franmaggi73@gmail.com>
2025-04-16 13:44:58 -0300

    :bug: fixed 404 error

--- src/lib/components/SideBar.svelte
+++ src/lib/components/SideBar.svelte
@@ -71,7 +71,7 @@
       <div><div class="divider m-0 p-0" /></div>
       <li><a href="/weather">Weather App</a></li>
       <div><div class="divider m-0 p-0" /></div>
-      <li><a href="/tournament">Tournament</a></li>
+      <li><a href="/Tournament">Tournament</a></li>
       <div><div class="divider m-0 p-0" /></div>
       <li><a href="/ecommerce">Ecommerce</a></li>
     </ul>
